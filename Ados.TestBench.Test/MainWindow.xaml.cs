1dc7abf455234422d027d166da7ef03ebff1285b
Lee JaeJung <ggelgod@gmail.com>
2016-05-03 23:48:09 +0900

    LIN Manager, log, state 모델 등 추가 메인 윈도우 동작 연결.

--- Ados.TestBench.Test/MainWindow.xaml.cs
+++ Ados.TestBench.Test/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.Windows.Threading;
 
 namespace Ados.TestBench.Test
 {
@@ -28,10 +29,75 @@ namespace Ados.TestBench.Test
             _manualPage = new ManualPage();
             _autoPage = new AutoPage();
 
+            DispatcherTimer timer = new DispatcherTimer()
+            {
+                Interval = new TimeSpan(100 * 10000),
+            };
+            timer.Tick += Transfer_Tick;
+            timer.Start();
+
+
             _modePages.Navigate(_manualPage);
+
+            if (_devList.Items.Count > 0)
+                _devList.SelectedIndex =
+                    (_devList.Items.Count > Properties.Settings.Default.DeviceIdx)
+                    ? Properties.Settings.Default.DeviceIdx : 0;
+
         }
+
+
+        private void Transfer_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.Now.Ticks;
+
+            if (LinManager.IsRxError)
+            {
+                _rxLamp.Fill = (Brush)this.Resources["rampError"];
+            }
+            else
+            {
+                _rxLamp.Fill = LinManager.RxTics > now ?
+                    (Brush)this.Resources["rampActive"] : (Brush)this.Resources["rampIdle"];
+            }
+            if (LinManager.IsTxError)
+            {
+                _txLamp.Fill = (Brush)this.Resources["rampError"];
+            }
+            else
+            {
+                _txLamp.Fill = LinManager.TxTics > now ?
+                    (Brush)this.Resources["rampActive"] : (Brush)this.Resources["rampIdle"];
+            }
+        }
+
+        private void refresh_Click(object sender, RoutedEventArgs e)
+        {
+            model.RefreshDevice();
+        }
+
+        private void cbDevices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            model.LinMgr.Device = (LinDevice)_devList.SelectedItem;
+        }
+
+        private void outMsg_Loaded(object sender, RoutedEventArgs e)
+        {
+            _outMsg.ItemsSource = model.LogsData;
+        }
+
+
+        ControllerModel model { get { return (ControllerModel)this.DataContext; } }
 
         ManualPage _manualPage;
         AutoPage _autoPage;
+
+        private void testMode_changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (_modeNavigation.SelectedIndex == 0)
+                _modePages.Navigate(_manualPage);
+            else
+                _modePages.Navigate(_autoPage);
+        }
     }
 }
